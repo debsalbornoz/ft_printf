aee8fcdc5d6af0c26bbfa1192e9f293f466f94ce
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2023-10-30 20:17:20 -0400

    Update ft_print_decimal.c

--- ft_print_decimal.c
+++ ft_print_decimal.c
@@ -15,43 +15,49 @@
 int	count_base10(int nbr)
 {
 	unsigned int	i;
-
 	i = 1;
-	if (nbr < 0)
+
+	if (nbr < 0)  // If the number is negative, account for the negative sign
 	{
 		nbr *= -1;
 		i++;
 	}
+
+	// Count the number of digits using integer division by 10
 	while (nbr > 9)
 	{
 		nbr = nbr / 10;
 		i++;
 	}
-	return (i);
+	return (i);  // Return the total number of digits
 }
 
 int	ft_putnbr(long int n)
 {
-	char	c;
-	int		ft_count;
+	char	c;          // Variable to store digits as characters
+	int		ft_count;   // Total number of characters printed (including negative sign and digits)
 
-	ft_count = count_base10(n);
+	ft_count = count_base10(n);  // Count the number of digits in the integer
+
 	if (n == -2147483648)
 	{
-		write(1, "-2147483648", 11);
+		write(1, "-2147483648", 11);  // Handle special case for INT_MIN
 		return (11);
 	}
-	c = n % 10 + '0';
-	if (n < 0)
+	
+	c = n % 10 + '0';  // Convert the last digit to a character
+	if (n < 0)  // If the number is negative, print the negative sign and negate n
 	{
 		write(1, "-", 1);
 		n = -n;
-		c = n % 10 + '0';
+		c = n % 10 + '0';  // Update the character representation of the last digit
 	}
-	if (n >= 10)
+	
+	if (n >= 10)  // Recursively print the remaining digits
 	{
 		ft_putnbr(n / 10);
 	}
-	write(1, &c, 1);
-	return (ft_count);
+	
+	write(1, &c, 1);  // Print the current digit
+	return (ft_count);  // Return the total number of characters printed
 }
