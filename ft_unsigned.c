a4e91e3887ef38b4ddc47129d3284c3d1dfb1ef5
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2023-10-30 20:26:10 -0400

    Update ft_unsigned.c

--- ft_unsigned.c
+++ ft_unsigned.c
@@ -14,26 +14,26 @@
 
 int	count_unsigned(unsigned int nbr)
 {
-	unsigned int	i;
+	unsigned int	i;  // Counter for digits
 
 	i = 1;
-	while (nbr > 9)
+	while (nbr > 9)  // While the number has more than one digit
 	{
-		nbr = nbr / 10;
-		i++;
+		nbr = nbr / 10;  // Divide by 10 to remove the last digit
+		i++;  // Increment digit count
 	}
-	return (i);
+	return (i);  // Return the total number of digits in the integer
 }
 
 int	ft_unsigned(unsigned int n)
 {
-	char	c;
+	char	c;  // Variable to store digits as characters
 
-	c = n % 10 + '0';
-	if (n >= 10)
+	c = n % 10 + '0';  // Convert the last digit to a character
+	if (n >= 10)  // If the number has more than one digit, print the remaining digits recursively
 	{
 		ft_putnbr(n / 10);
 	}
-	write(1, &c, 1);
-	return (count_unsigned(n));
+	write(1, &c, 1);  // Print the current digit
+	return (count_unsigned(n));  // Return the total number of characters printed for the unsigned integer
 }
