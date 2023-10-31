2e96b3e1c74d92fb3df4f9adef37f1f08cf37933
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2023-10-30 20:24:31 -0400

    Update ft_putstr.c

--- ft_putstr.c
+++ ft_putstr.c
@@ -17,15 +17,15 @@ int	ft_putstr(char *str)
 	int	i;
 
 	i = 0;
-	if (str == NULL)
+	if (str == NULL)  // If the input string is NULL, print "(null)"
 	{
 		write(1, "(null)", 6);
-		return (6);
+		return (6);  // Return 6, indicating 6 characters have been printed
 	}
-	while (str[i] != '\0')
+	while (str[i] != '\0')  // Loop through the string until the null terminator is encountered
 	{
-		ft_putchar(str[i]);
-		i++;
+		ft_putchar(str[i]);  // Print each character of the string
+		i++;  // Move to the next character
 	}
-	return (i);
+	return (i);  // Return the total number of characters printed in the string
 }
